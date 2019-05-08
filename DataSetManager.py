19fb277321e575a351aff8fac48b98e6a3f9722f
Eric Soubigou <ericsoubigou@gmail.com>
2019-05-08 13:34:04 -0400

    Code polishing

--- DataSetManager.py
+++ DataSetManager.py
@@ -28,16 +28,6 @@ def create_data_set(dataset_param_dict):
         nb_off_carriers=dataset_param_dict["modulation"]["off_carrier"],
     )
 
-    # Creation of the receiver
-    receiver = Receiver(
-        cp_len=dataset_param_dict["modulation"]["cp_length"],
-        nb_carriers=dataset_param_dict["modulation"]["nb_carriers"],
-        modulation_order=dataset_param_dict["modulation"]["modulation_order"],
-        trellis=trellis,
-        nb_off_carriers=dataset_param_dict["modulation"]["off_carrier"],
-        equalizer_type=dataset_param_dict["equalizer"],
-    )
-
     # Creation of the AWGN Channel
     channel = Channel(
         mean=0,
